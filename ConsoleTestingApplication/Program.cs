fd7fad95d58321e38dc6981c3046394b2c407c2d
dkweb <dkwebdeveloper@gmail.com>
2022-02-01 12:10:46 +0530

    Add project files.

---
+++ ConsoleTestingApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleTestingApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a1 = "\t";
+            bool xx = string.IsNullOrWhiteSpace(a1);
+
+            Console.WriteLine("Hello World!" + xx);
+        }
+    }
+}
